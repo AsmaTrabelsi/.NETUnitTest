44838325cd8397362320a2fa64b1596e20392403
asma <asmatra149@gmail.com>
2023-07-01 21:38:37 +0100

    first MS test

---
+++ Sparky.MSTest/CalculatorMSTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sparky;
+
+namespace Sparky.MSTest
+{
+    [TestClass]
+    public  class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // Arrange : 
+
+            Calculator calc = new();
+            // Act
+            var result = calc.AddNumbers(1,2);
+
+            // Assert 
+            Assert.AreEqual(3,result);
+
+        }
+    }
+}
